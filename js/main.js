067df27636c2c3a8defbdd89d87da2a07c610412
web-kk <308552493@qq.com>
2016-11-02 12:47:28 +0800

    80

--- js/main.js
+++ js/main.js
@@ -33,7 +33,8 @@ $(document).ready(function(){
 		var todo={
 			name:val,
 			states:0,
-			date:fullTime
+			date:fullTime,
+			color:'#f7f7f7'
 		}
 		$('.show').hide()
 		todos.push(todo)
@@ -41,4 +42,63 @@ $(document).ready(function(){
 		$('<li><div class="content">'+todo.name+'</div><div class="data">'+fullTime+'</div></li>').appendTo('#ul')
 		$('.show-in input').val('')
 	})
+	var oldText
+	
+	//
+	$('#ul').on('touchend','li',function(){
+		var index=$(this).index()
+		oldText=$(this).find('.content').html()	
+		$('#ul li').eq(index).remove()
+		todos.splice(index,1)
+		localStorage.todos=JSON.stringify(todos)
+		$('.show').show()
+		$('.show-in input').val(oldText)	
+		$('.s-data').html(fullTime)
+		var val=$.trim($('.show-in input').val())
+		var todo={
+			name:val,
+			states:0,
+			date:fullTime,
+			color:'#f7f7f7'
+		}
+		localStorage.todos=JSON.stringify(todos)		
+	})
+	//
+	$('.s-img').on('touchend',function(){
+		$('.show').hide()
+		var todo={
+			name:oldText,
+			states:0,
+			date:fullTime,
+			color:'#f7f7f7'
+		}
+		todos.push(todo)
+		localStorage.todos=JSON.stringify(todos)
+		$('<li><div class="content">'+todo.name+'</div><div class="data">'+fullTime+'</div></li>').appendTo('#ul')
+	})
+	//删除
+	$('.dele').on('touchend',function(){
+		$('.show').hide()
+		var index=$(this).index()
+		todos.splice(index,1)
+		localStorage.todos=JSON.stringify(todos)
+	})
+	//color
+	$('.s-background').on('touchend',function(){
+		if($('.color').css('display')=='none'){
+			$('.color').show()
+		}else{
+			$('.color').hide()
+		}
+	})
+	var color=['#f7f7f7','#A1D6AE','#F39484','#F7D885']
+	$('.color').on('touchend','li',function(){
+		var index=$(this).index()
+		$('.color li span').css('border-radius','0%').eq(index).css('border-radius','50%')
+		$('.show').css('background',color[index])
+		var length=$('#ul li').length
+		$('#ul li').eq(length+1).css('background',color[index])
+	})
 })
+
+
